2f1a6b528f02204c061ffc870352f7f9a7b33d6c
Desmend Jetton <desmendjetton@gmail.com>
2019-10-03 00:05:06 -0500

    updating subjects to see if this will pass error

--- class_central_spider/spiders/subjects.py
+++ class_central_spider/spiders/subjects.py
@@ -8,8 +8,8 @@ from scrapy.http import Request
 
 class SubjectsSpider(scrapy.Spider):
     name = 'subjects'
-    allowed_domains = ['class-central.com']
-    start_urls = ('http://class-central.com/subjects',)
+    allowed_domains = ['classcentral.com']
+    start_urls = ('http://classcentral.com/subjects',)
 
     def __init__(self, subject=None):
         self.subject = subject
@@ -23,6 +23,12 @@ class SubjectsSpider(scrapy.Spider):
             self.logger.info('Scraping all subjects')
             subjects = response.xpath(
                 '//*[@class="text--blue"]/@href').extract()
+            for subject in subjects:
+                yield Request(response.urljoin(subject), callback=self.parse_subject)
 
     def parse_subject(self, response):
-        pass
+        subject_name = response.xpath('//title').extract_first().split(' | ')[0]
+        subject_name = subject_name.split(' | ')
+        subject_name = subject_name[0]
+
+        courses = response.xpath('//*[@class="course-name-text text--bold"]')
